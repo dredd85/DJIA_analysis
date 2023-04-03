4a2082a32ec6f3dd09dea3e67db566ea2fd9a4c6
dredd85 <129594776+dredd85@users.noreply.github.com>
2023-04-03 10:36:18 +0200

    Update 1.open_csv.py

--- 1.open_csv.py
+++ 1.open_csv.py
@@ -3,4 +3,6 @@ import csv
 with open('DJIA.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
-        print(', '.join(row))
+        row = row[0]
+        split_row = row.split(",") #now we have list with two values
+        print(split_row) 
