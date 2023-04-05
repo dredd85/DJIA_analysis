b9598b5f318aaa7755d48c21f495c967f73e3555
Piotr Siergiej <129594776+dredd85@users.noreply.github.com>
2023-04-05 15:48:59 +0200

    Update 1.open_csv.py

--- 1.open_csv.py
+++ 1.open_csv.py
@@ -1,8 +1,23 @@
 import csv
+import sqlite3
 
-with open('DJIA.csv', newline='') as csvfile:
+fstock = input('Choose a csv file to upload:')
+if len(fstock) < 1:
+    print('File not loaded')
+    exit()
+
+conn = sqlite3.connect('DJIA.sqlite')
+cur = conn.cursor()
+
+print('Connected to database')
+cur.execute('CREATE TABLE IF not EXISTS stocks (Day DATE, Price INT)')
+
+with open(fstock, newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    print('Inserting data will begin shortly')
+    
     for row in spamreader:
-        row = row[0]
+        row = row[0]        
         split_row = row.split(",") #now we have list with two values
-        print(split_row) 
+        dat = split_row[0][1:-1]
+        val = split_row[1]
