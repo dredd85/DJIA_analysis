3ce69ecaa9622ce336c3d89f1b15fc2a862f02b9
Piotr Siergiej <129594776+dredd85@users.noreply.github.com>
2023-04-11 15:09:17 +0200

    Update 1.open_csv.py
    Selecting and inserting data from the CSV file to the SQLite database

--- 1.open_csv.py
+++ 1.open_csv.py
@@ -21,3 +21,18 @@ with open(fstock, newline='') as csvfile:
         split_row = row.split(",") #now we have list with two values
         dat = split_row[0][1:-1]
         val = split_row[1]
+        
+        if len(dat) < 1: continue              
+        cur.execute('SELECT Day from stocks where Day = ?', (dat,))
+        line = cur.fetchone()
+        #checking if a record alredy exists
+        if line is None:
+            print('Inserting: Day ', dat,'Price ', val)
+            cur.execute('INSERT INTO stocks(Day, Price) VALUES (?, ?)', (dat, val,))
+            #print inserting data
+        else:
+            print('Record already exists')
+        conn.commit()
+  
+print('Done')
+conn.close()
