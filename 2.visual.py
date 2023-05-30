5338a801237fb997cc3e2536442328bcd62ff6bb
Dredd85 <piotr.siergiej@gmail.com>
2023-05-30 15:36:47 +0200

    Made the chart more readable, added another MA and some annotations

--- 2.visual.py
+++ 2.visual.py
@@ -3,10 +3,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 conn = sqlite3.connect('DJIA.sqlite')
-query = "SELECT strftime('%Y %m', Day) as Month, avg(Price) OVER(ORDER BY Day ROWS BETWEEN 25 PRECEDING AND CURRENT ROW ) as AVG, Price FROM stocks group by month"
-data = pd.read_sql_query(query, conn)
+query = "SELECT strftime('%Y %m', Day) as Month, avg(Price) as Price FROM stocks GROUP BY Month"
+df = pd.read_sql_query(query, conn)
 
-plt.plot(data.Month, data.Price, data.AVG)
-plt.title("Dija Stock")
+ma_period25 = 25
+ma_period50 = 50
+
+df['Month'] = pd.to_datetime(df['Month'])  # Convert 'Month' column to datetime format
+df['MA25'] = df['Price'].rolling(window=ma_period25).mean()
+df['MA50'] = df['Price'].rolling(window=ma_period50).mean()
+
+plt.plot(df['Month'], df['Price'], label='Stock Price')
+plt.plot(df['Month'], df['MA25'], label=f'{ma_period25}-Moving Average', color='red')
+plt.plot(df['Month'], df['MA50'], label=f'{ma_period50}-Moving Average', color='green')
+
+# Find the index of the annotation point
+dotcom_bubble_burst_index = df[df['Month'].dt.year == 2000].index[0]
+globa_financial_crisis_index = df[df['Month'].dt.year == 2008].index[0]
+
+plt.annotate('Dot-com Bubble Burst', 
+            xy=(df['Month'][dotcom_bubble_burst_index], df['Price'][dotcom_bubble_burst_index]),
+            xytext=(df['Month'][dotcom_bubble_burst_index] - pd.DateOffset(years=6), df['Price'][dotcom_bubble_burst_index]+1000),
+            arrowprops=dict(arrowstyle='->'))
+
+plt.annotate('Global Finacial Criss', 
+            xy=(df['Month'][globa_financial_crisis_index], df['Price'][globa_financial_crisis_index]+900),
+            xytext=(df['Month'][globa_financial_crisis_index] - pd.DateOffset(years=7), df['Price'][globa_financial_crisis_index]+1500),
+            arrowprops=dict(arrowstyle='->'))
+
+plt.grid(True)
+plt.xlabel('Years')
+plt.xticks(df['Month'][::12], df['Month'].dt.year[::12], rotation=45)
+plt.ylabel('Price')
+plt.title('DJIA Stock Data with Moving Average')
+plt.legend()
+
 plt.show()
 conn.close()
